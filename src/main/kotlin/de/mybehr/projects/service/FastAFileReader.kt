0d09dd944d75a7eba62fa682f514765dcadb1d3a
Tobias Behr <tobias.behr@holisticon.de>
2017-01-15 09:20:15 +0100

    adds test for FastAFileReader

--- src/main/kotlin/de/mybehr/projects/service/FastAFileReader.kt
+++ src/main/kotlin/de/mybehr/projects/service/FastAFileReader.kt
@@ -2,8 +2,10 @@ package de.mybehr.projects.service
 
 import de.mybehr.projects.model.FastAFile
 import de.mybehr.projects.model.FileFormat
+import org.biojava.nbio.core.sequence.ProteinSequence
 import org.biojava.nbio.core.sequence.io.FastaReaderHelper
 import java.io.File
+import java.util.*
 
 /**
  * @author Tobias Behr
@@ -14,11 +16,15 @@ class FastAFileReader : FileReader<FastAFile> {
 
         val proteinSequences = FastaReaderHelper.readFastaProteinSequence(file)
 
+        val entries = extractFastAEntries(proteinSequences)
+        return FastAFile(file.absolutePath, entries)
+    }
+
+    internal fun extractFastAEntries(proteinSequences: LinkedHashMap<String, ProteinSequence>): ArrayList<FastAFile.FastAEntry> {
         val entries = proteinSequences.entries
                 .map { e -> FastAFile.FastAEntry(e.key, e.value.sequenceAsString) }
                 .toCollection(arrayListOf())
-        return FastAFile(file.absolutePath, entries)
-
+        return entries
     }
 
 }
---
+++ src/test/kotlin/de/mybehr/projects/service/FastAFileReaderTest.kt
@@ -0,0 +1,31 @@
+package de.mybehr.projects.service
+
+import org.assertj.core.api.Assertions.assertThat
+import org.biojava.nbio.core.sequence.ProteinSequence
+import org.junit.Test
+
+/**
+ * @author Tobias Behr
+ */
+class FastAFileReaderTest {
+
+    val reader : FastAFileReader = FastAFileReader()
+
+    @Test
+    fun shouldExtractFastAEntriesFromResult() {
+        val fastAEntries = reader.extractFastAEntries(linkedMapOf("Header1" to ProteinSequence("AAAAA"), "Header2" to ProteinSequence("DDDDD")))
+
+        assertThat(fastAEntries).hasSize(2)
+        assertThat(fastAEntries[0].header).isEqualTo("Header1")
+        assertThat(fastAEntries[0].sequence).isEqualTo("AAAAA")
+        assertThat(fastAEntries[1].header).isEqualTo("Header2")
+        assertThat(fastAEntries[1].sequence).isEqualTo("DDDDD")
+    }
+
+    @Test
+    fun shouldNotFailOnEmptyMap() {
+        val fastAEntries = reader.extractFastAEntries(linkedMapOf())
+
+        assertThat(fastAEntries).isEmpty()
+    }
+}
