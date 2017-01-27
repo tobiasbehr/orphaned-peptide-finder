484fc5f7c35442f67ca29cecfbe09ebfcf8357ee
Tobias Behr <tobias.behr@holisticon.de>
2017-01-27 21:57:20 +0100

    return correct result from NewOrfService

--- src/main/kotlin/de/mybehr/projects/service/analysis/NewOrfService.kt
+++ src/main/kotlin/de/mybehr/projects/service/analysis/NewOrfService.kt
@@ -8,17 +8,19 @@ import de.mybehr.projects.neworf.NewOrfInput
  */
 class NewOrfService(val input: NewOrfInput) {
 
-    val toPeptidesInput: (Array<String>) -> PeptideInput = { row -> PeptideInput(row[input.peptidesColumn.index], row[input.accessionColumn.index])}
+    internal val toPeptidesInput: (Array<String>) -> PeptideInput = { row -> PeptideInput(row[input.peptidesColumn.index], row[input.accessionColumn.index])}
 
-    val isInFastAEntries: (String, List<FastAFile.FastAEntry>) -> Boolean = { peptide, fastAEntries -> fastAEntries.filter { it.sequence.contains(peptide) }.any()}
+    internal val isInFastAEntries: (String, List<FastAFile.FastAEntry>) -> Boolean = { peptide, fastAEntries -> fastAEntries.filter { it.sequence.contains(peptide) }.any()}
 
-    fun findNewPeptides() : List<String> {
+    fun findNewPeptides() : List<Array<String>> {
         val peptideInputs: List<PeptideInput>? = input.peptidesFile?.content?.map(toPeptidesInput)
         val fastAEntries: List<FastAFile.FastAEntry>? = input.fastAFile?.entries
 
-        peptideInputs?.filterNot { isInFastAEntries.invoke(it.peptide, fastAEntries!!) }?.forEach(::println)
-        return arrayListOf()
+        return peptideInputs?.
+                filterNot { isInFastAEntries.invoke(it.peptide, fastAEntries!!) }?.
+                map { p -> arrayOf(p.peptide, p.accession) }?.
+                toList().orEmpty()
     }
 
-    data class PeptideInput(val peptide: String, val accession: String)
+    internal data class PeptideInput(val peptide: String, val accession: String)
 }
